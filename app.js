c126bcfc16c3977538d332205e7fa31c43b890dd
zeke farioli <efarioli@gmail.com>
2017-01-25 10:26:41 +0000

    adding routing html5 mode

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 angular.module('myFirstApp', ['ngRoute'])
-.config(['$routeProvider', function ($routeProvider) {
+.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
   $routeProvider
   .when('/about', {templateUrl: 'views/about.html'})
   .when('/contact', {templateUrl: 'views/contact.html'} )
   .otherwise({redirectTo: '/'})
+  $locationProvider.html5Mode(true);
 }])
 .factory('personService', function(){
 
